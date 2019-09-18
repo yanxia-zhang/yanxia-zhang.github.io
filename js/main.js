7287c6ea1fa9a4b50e8a49f6fc9c98bbb5365042
Ming Ki Chong <unexpected.exception@gmail.com>
2019-09-17 23:30:28 -0700

    add publications and software sections

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+  if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
     document.getElementById("avatar").style.width = "85px";
   } else {
     document.getElementById("avatar").style.width = "150px";
